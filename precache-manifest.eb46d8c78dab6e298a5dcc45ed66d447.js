96a57e38c35a5fc0eb3b0759e838d5c8d39ad58d
pauldavid23 <50937998+pauldavid23@users.noreply.github.com>
2020-07-25 19:19:17 +0800

    Updates

---
+++ precache-manifest.eb46d8c78dab6e298a5dcc45ed66d447.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c43c3f677e81f31ae678702d8bc2499d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f6e5f1b308bcff12cb31",
+    "url": "/monsters-rolodex/static/css/main.e54dc8ca.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6e5f1b308bcff12cb31",
+    "url": "/monsters-rolodex/static/js/main.0fa0587c.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
